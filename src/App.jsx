393243849b46b1778a23a8b2e1425969a33aaaf8
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-12 19:27:50 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Button from "./components/Button.jsx";
+
+const App = () => {
+
+
+	return (
+		<div className="App">
+			<h1 className="text-3xl">
+				App!!
+				<Button/>
+			</h1>
+		</div>
+	)
+}
+
+export default App
